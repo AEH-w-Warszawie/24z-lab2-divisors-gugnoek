71ded50832040d273e958b15971a1470f0f5c237
gugnoek <gugnoek2077_aehit@students.vizja.pl>
2024-10-20 13:45:58 +0100

    Update DivisorsLib.cpp

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -11,14 +11,14 @@ std::vector<unsigned long long> divisors(unsigned long long number)
 			result.push_back(i);
 	}
 	*/
-	for (unsigned long long i = 1; i * i <= number; ++i) // Sadece sqrt(number)'a kadar döngü
+	for (unsigned long long i = 1; i * i <= number; ++i)
 	{
 		if (number % i == 0)
 		{
-			result.push_back(i);  // i bir bölen
-			if (i != number / i)  // i ve number / i e?it de?ilse, çifti ekle
+			result.push_back(i);
+			if (i != number / i)
 			{
-				result.push_back(number / i);  // number / i de bölen
+				result.push_back(number / i);  // number / i de bÃ¶len
 			}
 		}
 	}
