ca23af48dda5780cc77f0ce1f4087804591b4e1f
gugnoek <gugnoek2077_aehit@students.vizja.pl>
2024-10-20 14:36:01 +0100

    Faster Algorythm

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -4,10 +4,23 @@
 std::vector<unsigned long long> divisors(unsigned long long number)
 {
 	std::vector<unsigned long long> result;
+	/*
 	for (unsigned long long i = 1; i <= number; ++i)
 	{
 		if (number % i == 0)
 			result.push_back(i);
+	}
+	*/
+	for (unsigned long long i = 1; i * i <= number; ++i) // Sadece sqrt(number)'a kadar döngü
+	{
+		if (number % i == 0)
+		{
+			result.push_back(i);  // i bir bölen
+			if (i != number / i)  // i ve number / i e?it de?ilse, çifti ekle
+			{
+				result.push_back(number / i);  // number / i de bölen
+			}
+		}
 	}
 	return result;
 }
